9a0df357b846743077d52a2503dc3f7d0a8f3021
Manwei <manwei.chan@gmail.com>
2020-11-03 17:28:11 -0500

    Adding two other forms of the rocket equation here that calculate both m0 and mf

--- propEqns.py
+++ propEqns.py
@@ -35,3 +35,33 @@ def delV_rocketEqn(isp, m0, mf, g0 = 9.80665):
 	"""
 	delV = isp * g0 * np.log(m0/mf)
 	return delV
+
+def m0_rocketEqn(mf, delV, isp, g0 = 9.80665):
+	"""
+	Calculate initial mass using the rocket eqn
+	Inputs (isp, delV, mf, g0)
+	isp  : specific impulse in seconds
+	delV : delta V of maneuver (m/s)
+	mf   : final mass (dry mass)
+	g0   : gravitational constant, default is 9.80665 m/s^2
+
+	Output
+	m0   : initial mass (kg)
+	"""
+	m0 = mf * np.exp(delV/(isp * g0))
+	return m0
+
+def mf_rocketEqn(m0, delV, isp, g0 = 9.80665):
+	"""
+	Calculate final mass using the rocket eqn
+	Inputs (isp, delV, m0, g0)
+	isp  : specific impulse in seconds
+	delV : delta V of maneuver (m/s)
+	m0   : initial mass (wet mass)
+	g0   : gravitational constant, default is 9.80665 m/s^2
+
+	Output
+	mf   : final mass (kg)
+	"""
+	mf = m0 * np.exp(-delV/(isp * g0))
+	return mf
