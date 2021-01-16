8a3a1ff195468e2938a123e8b65dc004303c3d3b
Manwei <manwei.chan@gmail.com>
2021-01-16 18:26:28 -0500

    Added link budget and Rf/Optical payloads for satClasses

--- satClasses.py
+++ satClasses.py
@@ -11,7 +11,8 @@ from poliastro.bodies import Earth
 from poliastro.frames.equatorial import GCRS
 
 class Satellite(Orbit):
-    def __init__(self, state, epoch, dataMem = None, schedule = None):
+    def __init__(self, state, epoch, dataMem = None, schedule = None, txOpticalPayload = None, rxOpticalPayload = None,
+    txRfPayload = None, rxRfPayload = None):
         super().__init__(state, epoch) #Inherit class for Poliastro orbits (allows for creation of orbits)
         
         if dataMem is None: #Set up ability to hold data
@@ -23,6 +24,26 @@ class Satellite(Orbit):
             self.schedule = []
         else:
             self.schedule = schedule
+
+        if txOpticalPayload is None: #Set up ability to hold an optical payload
+            self.txOpticalPayload = []
+        else:
+            self.txOpticalPayload = txOpticalPayload
+
+        if rxOpticalPayload is None: #Set up ability to hold an optical payload
+            self.rxOpticalPayload = []
+        else:
+            self.rxOpticalPayload = rxOpticalPayload
+
+        if txRfPayload is None: #Set up ability to hold an optical payload
+            self.txRfPayload = []
+        else:
+            self.txRfPayload = txRfPayload
+
+        if rxRfPayload is None: #Set up ability to hold an optical payload
+            self.rxRfPayload = []
+        else:
+            self.rxRfPayload = rxRfPayload
     
     def add_data(self, data): #Add data object to Satellite
         self.dataMem.append(data)
@@ -33,7 +54,118 @@ class Satellite(Orbit):
             
     def add_schedule(self, sch_commands): #Adds a schedule to the satellite
         self.schedule.append(sch_commands)
-        
+
+    def add_txOpticalPayload(self, txOpticalPL): #Add a tx optical payload
+        self.txOpticalPayload.append(txOpticalPL)
+
+    def add_rxOpticalPayload(self, rxOpticalPL): #Add an rx optical payload
+        self.rxOpticalPayload.append(rxOpticalPL)
+
+    def add_txRfPayload(self, txRfPL): #Add a tx optical payload
+        self.txRfPayload.append(txRfPL)
+
+    def add_rxRfPayload(self, rxRfPL): #Add an rx optical payload
+        self.rxRfPayload.append(rxRfPL)
+
+    def calc_RxLinkBudgetTx(self, RxObject, L_atm = 0, L_pointing = 0, L_pol, txID = 0, rxID = 0): 
+        """
+        Calculate link budget with self as transmitter
+
+        RxObject: Satellite or GroundStation Object
+        L_atm (dB): Atmospheric loss
+        L_pointint (dB): pointing loss
+        L_pol (dB): Polarization loss
+        txID: Which Tx payload to use. Default to 0 since we will most likely just have 1 payload
+        rxID: Which Rx payload to use. Default to 0 since we will most likely just have 1 payload
+        """
+        # try: 
+        assert hasattr(self, 'txRfPayload'), "Need to add txRFPayload to Transmitting Satellite"
+        txPL = self.txRfPayload[txID]
+        EIRP_dB = txPL.get_EIRP()
+        wl = txPL.wavelength #wavelength
+        if RxObject.__class__.__name__ == 'Satellite':
+            assert hasattr(RxObject, 'rxRfPayload'), "Need to add rxRFPayload to Transmitting Satellite"
+            rxPL = RxObject.rxRfPayload[rxID]
+            G_rx = rxPL.G_r
+            posVecTx = self.r
+            posVecRx = RxObject.r
+
+            posVecDiff = astropy.coordinates.CartesianRepresentation(posVecTx - posVecRx)
+            dist = posVecDiff.norm().to(u.m)
+
+
+            L_fs = (4 * np.pi * dist / wl) ** 2 #Free space path loss
+            L_fs_dB = 10 * np.log10(L_fs.value)
+            P_rx = EIRP_dB - L_fs_dB + G_rx - L_atm - L_pointing - L_pol
+
+        return P_rx
+        # except AttributeError:
+        #     print("Need to add txRFPayload to Transmitting Satellite")
+
+
+
+
+class TxOpticalPayload(): #Defines tx optical payload
+    def __init__(self, P_tx, wavelength, beam_width, pointingErr = 0, sysLoss = 0): # Add tx optical payload
+        """
+        Defines an optical payload tx
+        P_tx (W): Transmit power
+        wavelength (m): wavelength in meters
+        beam_width (rad): FWMH, Beam width
+        pointingErr (rad): pointing error
+        sysLoss (dB): System loss
+        """
+        self.P_tx = P_tx
+        self.wavelength = wavelength
+        self.beam_width = beam_width
+        self.pointintErr = pointingErr
+        self.sysLoss = sysLoss
+
+class RxOpticalPayload(): #Defines tx optical payload
+    def __init__(self, aperture, sysLoss = 0): # Add tx optical payload
+        """
+        Defines an optical payload rx
+        aperture (m): aperture diameter
+        sysLoss (dB): System loss
+        """
+        self.aperature = aperture
+        self.sysLoss = sysLoss
+
+class TxRfPayload(): #Define EIRP of the txRFPayload
+    def __init__(self, P_tx, G_tx, wavelength, L_tx = 0):
+        """
+        Defines an RF tx payload
+        P_tx (W): Transmit power
+        G_tx (dB): Gain of the transmitter
+        wavelength (m): wavelength in meters
+        L_tx (dB): System loss
+        """
+        self.P_tx = P_tx
+        self.G_tx = G_tx
+        self.wavelength = wavelength
+        self.L_tx = L_tx
+
+    def get_EIRP(self):
+        P_tx = self.P_tx
+        P_tx_dB = 10 * np.log10(P_tx)
+
+        L_tx_dB = self.L_tx
+        G_tx_dB = self.G_tx
+
+        EIRP_dB = P_tx_dB + L_tx_dB + G_tx_dB
+        return EIRP_dB
+
+class RxRfPayload(): #Define RF receiver
+    def __init__(self, G_r, T_sys, L_line = 0):
+        """
+        Defines an RF rx payload
+        G_r (dB): Gain of the receiver
+        T_sys (K): Temperature of the system
+        L_line (dB): Line loss at receiver
+        """
+        self.G_r = G_r
+        self.T_sys = T_sys
+        self.L_line = L_line
 
 class Data():
     def __init__(self, tStamp, dSize):
@@ -108,7 +240,7 @@ class BurnSchedule():
         self.time = time
         self.deltaV = deltaV
         
-class xLinksSchedule():
+class XLinksSchedule():
     def __init__(self, time, direction):
         """
         Defnes crosslinks for schedules
@@ -118,7 +250,7 @@ class xLinksSchedule():
         self.time = time
         self.direction = direction
         
-class dLinksSchedule():
+class DLinksSchedule():
     def __init__(self, time, direction):
         """
         Defnes downlinks for schedules
@@ -128,7 +260,7 @@ class dLinksSchedule():
         self.time = time
         self.direction = direction
         
-class imageSchedule():
+class ImageSchedule():
     def __init__(self, time, direction):
         """
         Defnes image taking schedule
@@ -138,8 +270,9 @@ class imageSchedule():
         self.time = time
         self.direction = direction
         
-class groundStation():
-    def __init__(self, lon, lat, h, data = None):
+class GroundStation():
+    def __init__(self, lon, lat, h, data = None, txOpticalPayload = None, rxOpticalPayload = None,
+    txRfPayload = None, rxRfPayload = None):
         """
         Define a ground station. Requires astopy.coordinates.EarthLocation
         lat (deg): latitude 
@@ -158,12 +291,43 @@ class groundStation():
             self.data = []
         else:
             self.data = data
-            
+
+        if txOpticalPayload is None: #Set up ability to hold an optical payload
+            self.txOpticalPayload = []
+        else:
+            self.txOpticalPayload = txOpticalPayload
+
+        if rxOpticalPayload is None: #Set up ability to hold an optical payload
+            self.rxOpticalPayload = []
+        else:
+            self.rxOpticalPayload = rxOpticalPayload
+
+        if txRfPayload is None: #Set up ability to hold an optical payload
+            self.txRfPayload = []
+        else:
+            self.txRfPayload = txRfPayload
+
+        if rxRfPayload is None: #Set up ability to hold an optical payload
+            self.rxRfPayload = []
+        else:
+            self.rxRfPayload = rxRfPayload
     def propagate_to_ECI(self, obstime): #Gets GCRS coordinates (ECI)
         return self.loc.get_gcrs(obstime) 
     
     def get_ECEF(self):
         return self.loc.get_itrs()
+
+    def add_txOptical(self, txOpticalPL): #Add a tx optical payload
+        self.txOpticalPayload.append(txOpticalPL)
+
+    def add_rxOptical(self, rxOpticalPL): #Add an rx optical payload
+        self.rxOpticalPayload.append(rxOpticalPL)
+
+    def add_txRf(self, txRfPL): #Add a tx optical payload
+        self.txRfPayload.append(txRfPL)
+
+    def add_rxRf(self, rxRfPL): #Add an rx optical payload
+        self.rxRfPayload.append(rxRfPL)
     
     #Note ITRF is body fixed (ECEF)
         
