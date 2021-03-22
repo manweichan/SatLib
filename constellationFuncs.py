Added secondstage plane dispersion function to constellationFuncs. Based on Fregat launch analysis from before. Also fixed some of the orbital Mechanics functions so they work with astropy units

constellationFuncs.py
@@ -1,6 +1,12 @@
 import astropy
 import astropy.units as u
-from satClasses import *
+# from satClasses import *
+import satClasses as sc
+import matplotlib.pyplot as plt
+import propEqns as pe
+import orbitalMechanics as om
+import poliastro
+import numpy as np
 
 def genWalker(i, t, p, f, alt, epoch = False):
     """
@@ -44,10 +50,10 @@ def genWalker(i, t, p, f, alt, epoch = False):
             omega0 = plane * phaseDiff
             omega = omega0 + sat * interPlaneSpacing
             if epoch:
-                orbLoop = Satellite.circular(Earth, alt = alt,
+                orbLoop = sc.Satellite.circular(Earth, alt = alt,
                      inc = i, raan = raan, arglat = omega, epoch = epoch)
             else:
-                orbLoop = Satellite.circular(Earth, alt = alt,
+                orbLoop = sc.Satellite.circular(Earth, alt = alt,
                      inc = i, raan = raan, arglat = omega)
             planeSats.append(orbLoop)
         sats.append(planeSats)
@@ -56,3 +62,118 @@ def genWalker(i, t, p, f, alt, epoch = False):
 
 def getSatValues(constellation, value):
     return [[getattr(sat,value) for sat in plane] for plane in constellation]
+
+def secondStagePlaneDispersion(dryMass, wetMass, payloadMass, satelliteMass, satsPerPlane, loiterAlt, finalAlt, loiterInc, finalInc, isp,
+                                rPlanet = poliastro.constants.R_earth, plot = False, verbose = False):
+    """
+    Determines how many planes one can achieve using second stage/space tug burns
+
+    ** Assuming inputs with astropy units, astropy will handle unit conversions, otherwise inputs must be in the units specified below **
+    Inputs
+    dryMass (kg) : second stage dry mass
+    wetMass (kg) : second stage wet mass
+    payloadMass (kg) : second stage payload mass capacity
+    satelliteMass (kg) : mass of one satellite in constellation
+    satsPerPlane (int) : number of satellites in a plane
+    loiterAlt (m) : altitude of phasing orbit
+    finalAlt (m) : altitude of final orbit
+    loiterInc (rad) : inclination of phasing orbit
+    finalInc (rad) : inclination of final orbit
+    isp (s) : isp of rocket fuel used
+    rPlanet (m) : radius of planet (default is Earth)
+    plot (bool) : set to True for plotting
+    verbose (bool) : set to True for print statements
+
+    Outputs
+    deployedPlanes : Number of planes deployed
+    """
+    #Check for astropy classes
+    if not isinstance(dryMass, astropy.units.quantity.Quantity):
+        dryMass = dryMass * u.kg
+    if not isinstance(wetMass, astropy.units.quantity.Quantity):
+        wetMass = wetMass * u.kg
+    if not isinstance(payloadMass, astropy.units.quantity.Quantity):
+        payloadMass = payloadMass * u.kg
+    if not isinstance(satelliteMass, astropy.units.quantity.Quantity):
+        satelliteMass = satelliteMass * u.kg
+    if not isinstance(loiterAlt, astropy.units.quantity.Quantity):
+       loiterAlt = loiterAlt * u.m
+    if not isinstance(finalAlt, astropy.units.quantity.Quantity):
+       finalAlt = finalAlt * u.m
+    if not isinstance(loiterInc, astropy.units.quantity.Quantity):
+       loiterInc = loiterInc * u.rad
+    if not isinstance(finalInc, astropy.units.quantity.Quantity):
+       finalInc = finalInc * u.rad
+    if not isinstance(isp, astropy.units.quantity.Quantity):
+       isp = isp * u.s
+    if not isinstance(rPlanet, astropy.units.quantity.Quantity):
+       rPlanet = rPlanet * u.m
+
+    ## Constant definition
+    g0 = 9.80665 * u.m / u.s / u.s
+
+    initFuelMass = wetMass - dryMass
+    initMass = wetMass + payloadMass
+
+    possiblePlanes = np.floor(payloadMass/(satsPerPlane * satelliteMass))
+    massPerDrop = satsPerPlane * satelliteMass #Offloaded mass in payload
+    incChanges = loiterInc - finalInc #Inclination changes of maneuver
+
+    rLoiter = loiterAlt + rPlanet
+    rFinal = finalAlt + rPlanet
+    delV12_1 = om.delV_HohmannPlaneChange(rFinal, rLoiter, incChanges)
+    delV12_2 = om.delV_HohmannPlaneChange(rLoiter, rFinal, incChanges)
+    delV12 = delV12_1 + delV12_2 #Total delta V required to go to loiter orbit and return
+
+    fuelTracker = initFuelMass
+    maneuverTracker = 0
+    massTracker = initMass - massPerDrop #Assume insert at drop orbit
+    payloadTracker = payloadMass
+    massPlot = []
+    fuelPlot = []
+    payloadPlot = []
+    maneuverPlot = []
+    propPlot = []
+    while fuelTracker > 0 and maneuverTracker < possiblePlanes and massTracker > dryMass:
+        mProp = massTracker * (1 - np.exp(-delV12/(isp * g0)))
+        fuelTracker = fuelTracker - mProp #Track fuel storage
+        if fuelTracker < 0: #Ran out of fuel
+            if verbose:
+                print("Out of fuel")
+            break
+        massTracker = massTracker - mProp - massPerDrop #Track total mass
+        payloadTracker -= massPerDrop
+        maneuverTracker += 1
+        # import ipdb; ipdb.set_trace()
+        # fuelPlot = np.append(fuelPlot, fuelTracker)
+        # massPlot = np.append(massPlot, massTracker)
+        # payloadPlot= np.append(payloadPlot, payloadTracker)
+        # maneuverPlot = np.append(maneuverPlot, maneuverTracker)
+        # propPlot = np.append(propPlot, mProp)
+        fuelPlot.append(fuelPlot)
+        massPlot.append(massPlot)
+        payloadPlot.append(payloadPlot)
+        maneuverPlot.append(maneuverPlot)
+        propPlot.append(propPlot)
+        if verbose:
+            print("Mass Used Propellant: ", mProp)
+            print("Mass Fuel:            ", fuelTracker)
+            print("Mass Total:           ", massTracker)
+            print("Mass Payload:         ", payloadTracker)
+            print("Planes Delployed:     ", maneuverTracker + 1) #+1 because first deployment is free (start in desired orobit)
+    deployedPlanes = maneuverTracker + 1
+    return deployedPlanes
+    if plot:
+        plt.figure()
+        plt.plot(maneuverPlot[1:-1], massPlot[1:-1], 'o', label = "Total Mass")
+        plt.plot(maneuverPlot[1:-1], fuelPlot[1:-1], 'o', label = "Total Fuel")
+        plt.plot(maneuverPlot[1:-1], payloadPlot[1:-1], 'o', label = "Payload Mass")
+        plt.plot(maneuverPlot[1:-1], propPlot[1:-1], 'o', label = "Propulsion Used")
+        plt.legend()
+        plt.grid()
+        plt.title("Fregat Deployment Specs")
+        plt.xlabel("Maneuver")
+        plt.ylabel("Mass (kg)")
+
+
+

orbitalMechanics.py
@@ -62,7 +62,7 @@ def delV_circInc(v, i):
     return delV
 
 
-def circ2elip_Hohmann(a1, a2, muPlanet=poliastro.constants.GM_earth.value):
+def circ2elip_Hohmann(a1, a2, muPlanet=poliastro.constants.GM_earth):
     """
     Delta V required to go from circular orbit to elliptical
     circ2elip_Hohmann(a1, a2, muPlanet = 3.986e14)
@@ -70,11 +70,15 @@ def circ2elip_Hohmann(a1, a2, muPlanet=poliastro.constants.GM_earth.value):
     a2: semi major axis of larger ellipse (m) 
     muPlanet: Gravitation parameter (Earth default)
     """
+    if not isinstance(a1, astropy.units.quantity.Quantity):
+       a1 = a1 * u.m
+    if not isinstance(a2, astropy.units.quantity.Quantity):
+       a2 = a2 * u.m
     v1 = np.sqrt(muPlanet/a1) * (np.sqrt(2*a2/(a1 + a2)) - 1)
     return np.abs(v1)
 
 
-def elip2circ_Hohmann(a1, a2, muPlanet=poliastro.constants.GM_earth.value):
+def elip2circ_Hohmann(a1, a2, muPlanet=poliastro.constants.GM_earth):
     """
     Delta V required to go from elliptical orbit to circular orbit
     elip2circ_Hohmann(a1, a2, muPlanet = 3.986e14)
@@ -82,11 +86,15 @@ def elip2circ_Hohmann(a1, a2, muPlanet=poliastro.constants.GM_earth.value):
     a2: radii of arrival of final circular orbit (Assuming Hohmann) (m)
     muPlanet: Gravitation parameter (Earth default)
     """
+    if not isinstance(a1, astropy.units.quantity.Quantity):
+       a1 = a1 * u.m
+    if not isinstance(a2, astropy.units.quantity.Quantity):
+       a2 = a2 * u.m
     v2 = np.sqrt(muPlanet/a2) * (1 - np.sqrt(2*a1/(a1 + a2)))
     return np.abs(v2)
 
 
-def delV_Hohmann(a1, a2, muPlanet=poliastro.constants.GM_earth.value):
+def delV_Hohmann(a1, a2, muPlanet=poliastro.constants.GM_earth):
     """
     Delta V required to conduct a Hohmann Transfer
     delV_Hohmann(a1, a2, muPlanet = 3.986e14)
@@ -94,13 +102,17 @@ def delV_Hohmann(a1, a2, muPlanet=poliastro.constants.GM_earth.value):
     a2: radii of arrival of final circular orbit (Assuming Hohmann) (m)
     muPlanet: Gravitation parameter (Earth default)
     """
+    if not isinstance(a1, astropy.units.quantity.Quantity):
+       a1 = a1 * u.m
+    if not isinstance(a2, astropy.units.quantity.Quantity):
+       a2 = a2 * u.m
     v1 = circ2elip_Hohmann(a1, a2)
     v2 = elip2circ_Hohmann(a1, a2)
     delV = v1 + v2
     return delV
 
 
-def t_Hohmann(a1, a2, muPlanet=poliastro.constants.GM_earth.value):
+def t_Hohmann(a1, a2, muPlanet=poliastro.constants.GM_earth):
     """
     Time required to conduct a Hohmann Transfer
     t_Hohmann(a1, a2, muPlanet = 3.986e14)
@@ -108,11 +120,15 @@ def t_Hohmann(a1, a2, muPlanet=poliastro.constants.GM_earth.value):
     a2: semi major axis of desired circular orbit (m)
     muPlanet: Gravitation parameter (Earth default)
     """
+    if not isinstance(a1, astropy.units.quantity.Quantity):
+       a1 = a1 * u.m
+    if not isinstance(a2, astropy.units.quantity.Quantity):
+       a2 = a2 * u.m
     t = np.pi * np.sqrt((a1 + a2)**3 / (8 * muPlanet))
     return t
 
 
-def delV_HohmannPlaneChange(a1, a2, theta, muPlanet=poliastro.constants.GM_earth.value):
+def delV_HohmannPlaneChange(a1, a2, theta, muPlanet=poliastro.constants.GM_earth):
     """
     Calculates the delta-V value to conduct a combined plane change and Hohmann
     delV_HohmannPlaneChange(a1, a2, theta)
@@ -121,6 +137,14 @@ def delV_HohmannPlaneChange(a1, a2, theta, muPlanet=poliastro.constants.GM_earth
     theta: degrees of inclination change (rad)
     muPlanet: Gravitation parameter (Earth default)
     """
+
+    if not isinstance(a1, astropy.units.quantity.Quantity):
+       a1 = a1 * u.m
+    if not isinstance(a2, astropy.units.quantity.Quantity):
+       a2 = a2 * u.m
+    if not isinstance(theta, astropy.units.quantity.Quantity):
+       theta = theta * u.rad
+       
     a = (a1 + a2) / 2  # semi major axis of transfer ellipse
 
     # Determine which orbit to conduct an inclination change at (larger radius)
@@ -361,21 +385,25 @@ def orbitalPeriod_fromAlt(alt, rPlanet=constants.R_earth, muPlanet=poliastro.con
     return t
 
 
-def circVel_fromAlt(alt, rPlanet=constants.R_earth.to(u.m).value, muPlanet=poliastro.constants.GM_earth.value):
+def circVel_fromAlt(alt, rPlanet=constants.R_earth.to(u.m).value, muPlanet=poliastro.constants.GM_earth):
     """
     Inputs (altitude, radius of planet-Earth is default, Gravitation parameter of planet-Earth is default)
     alt unit is in meters
     """
+    if not isinstance(alt, u.quantity.Quantity):
+        alt = alt * u.m
     r = alt + rPlanet
     v = np.sqrt(muPlanet/r)
     return v
 
 
-def circVel_fromRad(r, muPlanet=poliastro.constants.GM_earth.value):
+def circVel_fromRad(r, muPlanet=poliastro.constants.GM_earth):
     """
     Inputs (orbit radius, Gravitation parameter of planet-Earth is default)
     alt unit is in meters
     """
+    if not isinstance(r, u.quantity.Quantity):
+        r = r * u.m
     v = np.sqrt(muPlanet/r)
     return v
 